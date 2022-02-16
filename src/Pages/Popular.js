ba1af321e348dc95976931ec404d0dde41f54010
unknown <clamy6@yahoo.fr>
2022-02-16 16:57:04 +0100

    stockage API

--- src/Pages/Popular.js
+++ src/Pages/Popular.js
@@ -1,6 +1,28 @@
 import React from "react";
 
 class Popular extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movies: [],
+    };
+  }
+  componentDidMount() {
+    // récupération API
+    fetch(
+      "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=a8b9f0e5cc95854f24e9e4d259fa6033"
+    )
+      //Après récupération stockage du résultat dans un Json
+      .then((res) => res.json())
+      //Après stockage, modification du state movies par la clé results
+      //console.log du res et du state après modification
+      .then((res) => {
+        console.log(res);
+        this.setState({ movies: res.results });
+        console.log("movies: ", this.state.movies);
+      });
+  }
   render() {
     return (
       <div>
